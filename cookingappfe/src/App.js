d116688221f294845446b5a4dacb62525be60a9d
Banskumansku <jan.lindholm@helsinki.fi>
2020-11-19 14:20:49 +0200

    Initial commit

---
+++ cookingappfe/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import Square from './Components/Square'
+
+
+function App() {
+
+  //at the start call all recipes etc
+  //place in Grid component -> figure out how to grid
+
+  return (
+    <div class="wrapper">
+      <div class="box box1">
+        <Square />
+        <Square />
+        <Square />
+        <Square />        
+        <Square />
+        <Square />
+        <Square />
+        <Square />        
+        <Square />
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ cookingappfe/src/Components/Square.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Square = () => {
+    return (<div class="nested">asd</div>)
+}
+
+export default Square
